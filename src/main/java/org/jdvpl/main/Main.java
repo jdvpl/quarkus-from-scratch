2be407acd46642e6d76272b97e14f0022e908d22
jdsuarez18 <juanda554242@gmail.com>
2025-03-14 13:55:17 -0500

    Implement custom Quarkus application entry point in Main class

--- src/main/java/org/jdvpl/main/Main.java
+++ src/main/java/org/jdvpl/main/Main.java
@@ -1,4 +1,21 @@
 package org.jdvpl.main;
 
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
 public class Main {
+    public static void main(String ...args){
+        Quarkus.run(CustomApp.class,args);
+    }
+    public  static class CustomApp implements QuarkusApplication{
+
+        @Override
+        public int run(String... args) throws Exception {
+            System.out.println("Runing from custom App");
+            Quarkus.waitForExit();
+            return 0;
+        }
+    }
 }
